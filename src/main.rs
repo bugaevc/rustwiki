034f130919d647e3f18facfe0e41980ac491908b
Sergey Bugaev <bugaevc@gmail.com>
2019-06-19 00:42:03 +0300

    Add the Page structure and its methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read, Write};
+use std::fs::File;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Page {
+    title: String,
+    body: String,
+}
+
+impl Page {
+    fn load(title: String) -> io::Result<Page> {
+        let file_name = format!("{}.txt", title);
+        let mut file = File::open(file_name)?;
+        let mut body = String::new();
+        file.read_to_string(&mut body)?;
+        Ok(Page { title, body })
+    }
+
+    fn save(&self) -> io::Result<()> {
+        let file_name = format!("{}.txt", self.title);
+        let mut file = File::create(file_name)?;
+        write!(file, "{}", self.body)
+    }
+}
+
+fn main() -> io::Result<()> {
+    let page = Page {
+        title: String::from("Test"),
+        body: String::from("This is a sample page"),
+    };
+    page.save()?;
+    let page = Page::load(String::from("Test"))?;
+    println!("{:#?}", page);
+    Ok(())
 }
